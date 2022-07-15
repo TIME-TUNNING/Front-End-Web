25d53818998dc813ec7ecc6c1f85b50f225b76c9
SEOYOUNG BAEK <baeksy6067@gmail.com>
2022-07-16 08:50:56 +0900

    navbar + article 수정

--- front-end-web/src/App.js
+++ front-end-web/src/App.js
@@ -1,18 +1,51 @@
+import React, { useState } from 'react';
 import Header from "components/Header";
 import Navbar from "components/Navbar";
-import Article from "components/Article";
+import ArticleIng from "components/ArticleIng";
+import ArticleEnd from "components/ArticleEnd";
+import Footer from "components/Footer";
 import styles from 'styles/App.module.css';
 
 function App() {
+  const [scheduleMode, setScheduleMode] = useState('ing');
+  const [modeState, setModeState] = useState('조율 진행중');
+  // const [fontColor, setFontColor] = useState('black');
+  // const [isClicked, setIsClicked] = useState(false);
+
+  let _article = <ArticleIng />
+
+  if (scheduleMode === 'ing') {
+    _article = <ArticleIng />;
+  } else if (scheduleMode === 'end') {
+    _article = <ArticleEnd />;
+  }
+
   return (
     <div className="App">
       <Header />
       <section className={styles.main_body}>
-        <Navbar />
-        {/* <Article /> */}
+        <Navbar
+        modeState={modeState}
+        // fontColor={fontColor}
+        onChangeScheduleMode = {function(mode) {
+          setScheduleMode(mode);
+        }}
+        onChangeModeState = {function(mode) {
+          setModeState(mode);
+        }}
+        // onChangeFontColor = {function() {
+        //   setIsClicked(!isClicked);
+        //   if (isClicked === true) {
+        //     setFontColor('#4B77F2');
+        //   } else {
+        //     setFontColor('black');
+        //   }
+        // }}
+        />
+        {_article}
       </section>
+      <Footer />
     </div>
-    
   );
 }
 
---
+++ front-end-web/src/components/ArticleIng.js
@@ -0,0 +1,14 @@
+import styles from 'styles/Article.module.css';
+
+const ArticleIng = () => (
+    <article className={styles.article}>
+        <span className={styles.text}>
+            아직 조율중인
+        </span>
+        <span className={styles.text}>
+            일정이 없어요
+        </span>
+    </article>
+);
+
+export default ArticleIng;
