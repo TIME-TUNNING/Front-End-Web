46d3682c0db33952c0dfbfd3dc7311ee2f313e12
SANGDONG <csd9969@gmail.com>
2022-08-10 20:12:45 +0900

    timetable추가

--- front-end-web/src/components/MySchedule.js
+++ front-end-web/src/components/MySchedule.js
@@ -1,4 +1,23 @@
 import styles from 'styles/MySchedule.module.css';
+import Slider from 'react-slick';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+
+const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 5,
+    slidesToScroll: 3
+};
+
+// const [content, setContent] = useState();
+
+// const handleClickButton = e => {
+//     const { name } = e.target;
+//     setContent(name);
+// };
 
 const MySchedule = (props) => {
     // console.log(props.schedule.id);
@@ -13,8 +32,51 @@ const MySchedule = (props) => {
                     <div className={styles.desc}>
                         {props.schedule[0].desc}
                     </div>
-                    <div>
-
+                    <div className={styles.slide}>
+                        <Slider {...settings}>
+                            <div className={styles.dayblock}>
+                                <span>
+                                    <span className={styles.monthfont}>6월</span>
+                                    <span className={styles.dayfont}>
+                                        <p>15</p>
+                                        목요일
+                                    </span>
+                                </span>
+                            </div>
+                            <div className={styles.dayblock}>
+                                <h3>1</h3>
+                            </div>
+                            <div className={styles.dayblock}>
+                                <h3>1</h3>
+                            </div>
+                            <div className={styles.dayblock}>
+                                <h3>1</h3>
+                            </div>
+                            <div className={styles.dayblock}>
+                                <h3>1</h3>
+                            </div>
+                            <div className={styles.dayblock}>
+                                <h3>1</h3>
+                            </div>
+                            <div className={styles.dayblock}>
+                                <h3>1</h3>
+                            </div>
+                            <div className={styles.dayblock}>
+                                <h3>1</h3>
+                            </div>
+                            <div className={styles.dayblock}>
+                                <h3>1</h3>
+                            </div>
+                            <div className={styles.dayblock}>
+                                <h3>1</h3>
+                            </div>
+                        </Slider>
+                    </div>
+                    <div className={styles.tablebox}>
+                    </div>
+                    <div className={styles.time}>
+                        <span>00:00 03:00 06:00 09:00 12:00 15:00 18:00 21:00 24:00</span>
+                        
                     </div>
                 </div>
                 <div className={styles.btns}>
